a8c52ceeee2ae386048aeab7398cdbf2a642b6b3
NicolasPandraud <nicolaspandraud@aol.com>
2021-06-01 23:49:14 +0200

    Update home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {
+  MatCarousel,
+  MatCarouselComponent,
+} from '@ngbmodule/material-carousel';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +11,60 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  breakpoint: number;
+  slides = [
+    {
+      image: '../../../assets/morpion.jpg',
+    },
+    {
+      image: '../../../assets/gameboardbg.jpg',
+    },
+    {
+      image: '../../../assets/gameboardbg2.jpg',
+    },
+  ];
+
   constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    switch (true) {
+      case window.innerWidth < 600:
+        this.breakpoint = 1;
+        break;
+      case window.innerWidth < 960:
+        this.breakpoint = 2;
+        break;
+      case window.innerWidth < 1280:
+        this.breakpoint = 3;
+        break;
+      case window.innerWidth < 1920:
+        this.breakpoint = 3;
+        break;
+      default:
+        this.breakpoint = 4;
+    }
+  }
 
   btnClick(route: string) {
     this.router.navigateByUrl(route);
+  }
+
+  onResize(event: any) {
+    switch (true) {
+      case event.target.innerWidth < 600:
+        this.breakpoint = 1;
+        break;
+      case event.target.innerWidth < 960:
+        this.breakpoint = 2;
+        break;
+      case event.target.innerWidth < 1280:
+        this.breakpoint = 3;
+        break;
+      case event.target.innerWidth < 1920:
+        this.breakpoint = 3;
+        break;
+      default:
+        this.breakpoint = 4;
+    }
   }
 }
